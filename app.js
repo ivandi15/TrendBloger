7c22e0ab0fc0dfe06f29ccf04fe3448d791dfd30
ivandi15 <ivandilukito16gmail.com>
2025-06-11 22:24:56 +0700

    Memperbaiki

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-registerSW();
+// Memanggil fungsi registerSW
+registerSW(); 
 
-// Registers a service worker
+// Fungsi async untuk mendaftarkan Service Worker
 async function registerSW() {
+  // Mengecek apakah browser mendukung fitur Service Worker
   if ('serviceWorker' in navigator) {
     try {
-      // Change the service worker URL to see what happens when the SW doesn't exist
+      // Mendaftarkan file "sw.js" sebagai Service Worker
       const registration = await navigator.serviceWorker.register("sw.js");       
     } catch (error) {
+      // Menangkap dan menampilkan error
       showResult("Error while registering: " + error.message);
     }    
   } else {
-      showResult("Service workers API not available");
+    // Menampilkan pesan jika Service Worker tidak didukung
+    showResult("Service workers API not available");
   }
-}; 
+}
 
+// Fungsi untuk menampilkan pesan
 function showResult(text) {
   document.querySelector("output").innerHTML = text;
 }
